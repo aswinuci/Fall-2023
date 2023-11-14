c6c582599a04726d79fb37aba8a54953f68f7c1b
Pushpa Madhavan <aswin@dhcp-10-8-036-006.mobile.reshsg.uci.edu>
2023-11-13 19:56:32 -0800

    Updated transpose code

--- PDC/hw5/hw5_granularity/transpose.c
+++ PDC/hw5/hw5_granularity/transpose.c
@@ -18,60 +18,73 @@ typedef struct
 {
     Mat *mat;
     unsigned int numElements;
-    unsigned int grains;
-    unsigned int lastIndexTransposed;
+    unsigned int swaps;
+    unsigned int lastSwappedIndex;
+    pthread_mutex_t *mutex;
 } ThreadData;
 
-pthread_mutex_t mutex;
 
-void *transposeMatrix(void *arg)
+void *transpose(void *arg)
 {
     ThreadData *data = (ThreadData *)arg;
     unsigned int n = data->mat->n;
-    unsigned int grains = data->grains;
-    unsigned int exchange_index = 0;
-    unsigned int exchg_end_index = 0;
+    unsigned int swaps = data->swaps;
+
+    unsigned int swapStart=0;
+    unsigned int swapEnd=0;
 
     while (1)
     {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(data->mutex);
 
-        exchange_index = data->lastIndexTransposed + 1;
-        if (exchg_end_index >= data->numElements - 1)
+        swapStart = data->lastSwappedIndex + 1;
+        if (swapEnd >= data->numElements - 1)
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(data->mutex);
             break;
         }
-        exchg_end_index = exchange_index + grains - 1;
-        if (exchg_end_index >= data->numElements)
+        swapEnd = swapStart + swaps - 1;
+        if (swapEnd >= data->numElements)
         {
-            exchg_end_index = data->numElements - 1;
+            swapEnd = data->numElements - 1;
         }
-        data->lastIndexTransposed = exchg_end_index;
-        pthread_mutex_unlock(&mutex);
-        while (exchange_index <= exchg_end_index)
+        data->lastSwappedIndex = swapEnd;
+        pthread_mutex_unlock(data->mutex);
+
+        while (swapStart <= swapEnd)
         {
-            int row = n - 2 - floor(sqrt(-8 * exchange_index + 4 * n * (n - 1) - 7) / 2.0 - 0.5);
-            int col = exchange_index + row + 1 - n * (n - 1) / 2 + (n - row) * ((n - row) - 1) / 2;
+            int row = n - 2 - floor(sqrt(-8 * swapStart + 4 * n * (n - 1) - 7) / 2.0 - 0.5);
+            int col = swapStart + row + 1 - n * (n - 1) / 2 + (n - row) * ((n - row) - 1) / 2;
             int temp = data->mat->ptr[row * n + col];
-            data->mat->ptr[row * n + col] = data->mat->ptr[col * n + row];
+            data->mat->ptr[row * n + col] = data->mat->ptr[col * n+ row];
             data->mat->ptr[col * n + row] = temp;
-            exchange_index++;
+            swapStart++;
         }
     }
     pthread_exit(NULL);
 }
 
-ThreadData initialize_thread(Mat *mat, unsigned int grains, pthread_mutex_t *mutex)
+ThreadData initialize_thread_data(Mat *mat, unsigned int swaps, pthread_mutex_t *mutex)
 {
     ThreadData data;
     data.mat = mat;
-    unsigned int n = mat->n;
+    int n = mat->n;
     data.numElements = ((n * n) - n) / 2;
-    data.grains = grains;
-    data.lastIndexTransposed = -1;
+    data.swaps = swaps;
+    data.lastSwappedIndex = -1;
     pthread_mutex_init(mutex, NULL);
+    data.mutex = mutex;
     return data;
+}
+
+void create_thread(pthread_t *worker, ThreadData *data)
+{
+    int creation_rc = pthread_create(worker, NULL, &transpose, (void *)data);
+    if (creation_rc)
+    {
+        printf("Error creating thread\n");
+        exit(-1);
+    }
 }
 
 // Take transpose of a square matrix
@@ -92,19 +105,13 @@ void mat_squaretransp_sequential(Mat *mat)
 void mat_squaretransp_parallel(Mat *mat, unsigned int grain, unsigned int thr)
 {
     pthread_mutex_t mutex;
+    ThreadData data = initialize_thread_data(mat, grain, &mutex);
     pthread_t threads[thr];
-    ThreadData data = initialize_thread(mat, grain, &mutex);
-    for (unsigned int i = 0; i < thr; i++)
+    for (unsigned int i = 0; i < thr; ++i)
     {
-        int creation_rc = pthread_create(&threads[i], NULL, &transposeMatrix, (void *)&data);
-        if (creation_rc)
-        {
-            printf("Error creating thread\n");
-            exit(-1);
-        }
+        create_thread(&threads[i], &data);
     }
-
-    for (unsigned int i = 0; i < thr; i++)
+    for (unsigned int i = 0; i < thr; ++i)
     {
         pthread_join(threads[i], NULL);
     }
